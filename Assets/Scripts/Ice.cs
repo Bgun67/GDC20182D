e7f8778e15d4cc34533c7061a8d05bf6fd195268
Braden Gunther <bradengunther@cmail.carleton.ca>
2018-11-12 13:32:24 -0500

    Edits to Attack System, Ice

---
+++ Assets/Scripts/Ice.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ice : MonoBehaviour {
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+	
+		rb = transform.root.GetComponent<Rigidbody>();
+		
+		
+		if (rb != null)
+		{
+			rb.isKinematic = true;
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void OnDestroy () {
+		if (rb != null)
+		{
+			rb.isKinematic = false;
+		}
+	}
+}
