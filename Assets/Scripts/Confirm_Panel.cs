6a6a5ad5ee448279de2700130b9137121f1b56dc
Braden Gunther <bradengunther@cmail.carleton.ca>
2019-10-16 13:50:00 -0400

    Menu created in the scene, need move to prefab

---
+++ Assets/Scripts/Confirm_Panel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Confirm_Panel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static void Create()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Menu : MonoBehaviour
+{   
+    public static Pause_Menu _instance;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _instance = this;
+        Pause_Menu.Resume();
+    }
+    static void Pause()
+    {
+        _instance.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public static void Resume()
+    {
+        Time.timeScale = 1f;
+        _instance.gameObject.SetActive(false);
+    }
+    public void QuitToMainMenu()
+    {
+
+
+    }
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
